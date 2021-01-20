68a33312789dddf43f150360f7c8f13090f27df0
Frenz86 <frenzy86@gmail.com>
2021-01-21 00:29:37 +0100

    ee

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_table_download_link(df):
     """
     val = to_excel(df)
     b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="Your_File.xlsx">Download Results </a>' # decode b'abc' => abc
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="Result.xlsx">Download Results </a>' # decode b'abc' => abc
 
 
 def main():
